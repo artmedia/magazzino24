1e861517d2ab8f073e842bc4b3f5ddb78337cd53
artmedia <info@artmedia.it>
2020-06-19 16:06:28 +0200

    Add files via upload

---
+++ Entities/ApiReturnedObj.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssemblyPWAArtmedia.Entities
+{
+    public class ApiReturnedObj
+    {
+        public bool success { get; set; }
+        public string errormessage { get; set; }
+        public int errorcode { get; set; }
+        public string uidoperazione { get; set; }
+        public int pagecount { get; set; }
+
+        //[JsonConverter(typeof(InfoToStringConverter))]
+        public object obj;
+
+        public int versioneprogramma { get; set; }
+        public int sottoversioneprogramma { get; set; }
+    }
+    
+}
---
+++ Services/AppData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssemblyPWAArtmedia.Services
+{
+    public class AppData
+    {
+        public int Age { get; set; }
+        public string Token { get; set; }
+    }
+}
